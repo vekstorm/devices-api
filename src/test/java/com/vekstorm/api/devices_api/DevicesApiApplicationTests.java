01942f2a3d2b3c115881c8e14869ed67d6c7f0ea
dteso <dtesodev@gmail.com>
2026-06-24 17:07:54 +0200

    initial commit

---
+++ src/test/java/com/vekstorm/api/devices_api/DevicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vekstorm.api.devices_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
